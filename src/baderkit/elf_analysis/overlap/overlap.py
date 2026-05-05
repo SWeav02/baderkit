e12efb930e8f6cd10e03d865d2c1cb06c9b07ca7
Sam Weaver <sweav@unc.edu>
2026-05-05 16:27:53 -0400

    core vs. lonepair bugfix

--- src/baderkit/elf_analysis/overlap/overlap.py
+++ src/baderkit/elf_analysis/overlap/overlap.py
@@ -781,6 +781,7 @@ class BasinOverlap(BaseElfAnalysis):
         self,
         core_tol=0.01,
         lone_pair_tol=0.05,
+        core_dist_tol=0.2
     ):
         # create tracker for which basins are part of each atoms core
         cores = np.full(len(self.local_maxima_frac), -1, dtype=np.int64)
@@ -789,8 +790,19 @@ class BasinOverlap(BaseElfAnalysis):
 
         for atom_idx in range(len(self.atom_shell_groups)):
             atom_shells = self.atom_shell_groups[atom_idx]
+            shell_dists = self.atom_average_shell_dists[atom_idx]
             local_overlap = self.qtaim_overlap_groups[atom_idx]
-            for shell in atom_shells:
+            for shell, dist in zip(atom_shells, shell_dists):
+                # BUGFIX: If there is only one basin in a shell, we base whether it is
+                # a core or lone-pair on its distance
+                if len(shell) == 1:
+                    if dist <= core_dist_tol:
+                        cores[shell[0]] = atom_idx
+                        continue
+                    else:
+                        lone_pairs[shell[0]] = atom_idx
+                        continue
+
                 overlap_fracs = local_overlap[shell][:, 2]
                 local_indices = local_overlap[shell][:, 0].astype(int)
                 # if all members of this shell are almost entirely owned by this
